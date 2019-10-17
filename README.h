9a6007864ab450557648d2b6369dea1fe9eb738a
isaacmujuni <isaacmujunitim@gmail.com>
2019-10-17 23:44:16 +0300

    Add README.md for Snake Game description

--- README.h
+++
@@ -1,3 +0,0 @@
-# Snake-game-
-// snake game written in C++
-A simple snake game you can play with computer keys W,A,S,D
---
+++ README.md
@@ -0,0 +1,3 @@
+# Snake Game
+
+A simple snake game written in C++, played with the W, A, S, D keys.
